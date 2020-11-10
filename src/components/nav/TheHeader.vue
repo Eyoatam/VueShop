01384b2493518e33cdf3fcaf9814067f7512ce18
Eyoatam <eyoatamtamirat7@gmail.com>
2020-11-10 11:15:22 +0300

    initialize appp

---
+++ src/components/nav/TheHeader.vue
@@ -0,0 +1,86 @@
+<template>
+	<header>
+		<h1>
+			<router-link to="/">VueShop</router-link>
+		</h1>
+		<nav>
+			<ul>
+				<li>
+					<router-link to="/products">Products</router-link>
+				</li>
+				<li>
+					<router-link to="/cart">Cart</router-link>
+					<base-badge mode="elegant">{{ cart.qty }}</base-badge>
+				</li>
+				<li v-if="isLoggedIn">
+					<router-link to="/admin">Admin</router-link>
+				</li>
+			</ul>
+		</nav>
+		<div>
+			<button v-if="!isLoggedIn" @click="login">Login</button>
+			<button v-if="isLoggedIn" @click="logout">Logout</button>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	inject: ['isLoggedIn', 'login', 'logout', 'cart'],
+};
+</script>
+
+<style scoped>
+header {
+	height: 5rem;
+	background-color: #eee;
+	margin: 0 10%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+
+ul {
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	display: flex;
+	justify-self: center;
+	align-items: center;
+}
+
+li {
+	margin: 0 1rem;
+}
+
+a {
+	text-decoration: none;
+	color: #333;
+	font-weight: bold;
+	border-bottom: 2px solid transparent;
+	padding-bottom: 0.25rem;
+}
+
+a:hover,
+a:active,
+a.router-link-active {
+	color: #0278ff;
+	border-color: #0278ff;
+}
+
+button {
+	outline: none;
+	font: inherit;
+	cursor: pointer;
+	padding: 0.5rem 1.5rem;
+	border: 1px solid #0278ff;
+	background-color: transparent;
+	color: #0278ff;
+	border-radius: 30px;
+}
+
+button:hover,
+button:active {
+	background-color: #c9dcf8;
+}
+</style>
---
+++ src/components/products/ProductItem.vue
@@ -0,0 +1,91 @@
+<template>
+	<li class="product">
+		<div class="product__data">
+			<div class="product__image">
+				<img :src="image" :alt="title" />
+			</div>
+			<div class="product__text">
+				<h3>{{ title }}</h3>
+				<base-badge mode="highlight" :no-margin-left="true">
+					<h4>${{ price }}</h4>
+				</base-badge>
+				<p>{{ description }}</p>
+			</div>
+		</div>
+		<div class="product__actions">
+			<button @click="addToCart">Add to Cart</button>
+		</div>
+	</li>
+</template>
+
+<script>
+export default {
+	inject: ['addProductToCart'],
+	props: ['id', 'image', 'title', 'price', 'description'],
+	methods: {
+		addToCart() {
+			this.addProductToCart({
+				id: this.id,
+				image: this.image,
+				title: this.title,
+				price: this.price,
+			});
+		},
+	},
+};
+</script>
+
+<style scoped>
+li {
+	margin: 1.5rem auto;
+	border-radius: 0.5em;
+	box-shadow: -8px -8px 8px 0 rgba(255, 255, 255, 1),
+		8px 8px 8px 0 rgba(0, 0, 0, 0.05);
+	padding: 1rem;
+}
+
+.product__data {
+	display: flex;
+}
+
+.product__image {
+	margin-right: 1rem;
+}
+
+.product__image img {
+	height: 10rem;
+	width: 10rem;
+	object-fit: cover;
+	border-radius: 5px;
+	/* box-shadow: 8px 8px 8px 0.1px rgba(0, 0, 0, 0.2); */
+}
+
+.product__text h3 {
+	margin: 0 0 0.5rem 0;
+}
+
+.product__text h4 {
+	margin: 0;
+}
+
+.product__actions {
+	text-align: center;
+}
+
+button {
+	outline: none;
+	font: inherit;
+	cursor: pointer;
+	background-color: #0278ff;
+	color: white;
+	border: 1px solid #0278ff;
+	padding: 0.5rem 1.5rem;
+	border-radius: 30px;
+}
+
+button:hover,
+button:active {
+	background-color: #0a46b4;
+	border-color: #0a46b4;
+}
+</style>
